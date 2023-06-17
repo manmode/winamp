aaa28929f3b575152d7ace8d23843419f7a3dddc
Hiralal Manmode <hiralal.manmode@oracle.com>
2023-06-17 01:38:35 -0400

    draft

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<main>
+  <div class="block flex-row border-2 h-screen w-96 justify-items-stretch">
+    <div class="flex border-2 bg-green-700 h-3">
+      <span class="text-xs title line w-1/5 relative">winamp</span>
+    </div>
+    <div class="border-2 bg-red-500 h-24">song-control</div>
+    <div class="border-2 h-36">equalizer</div>
+    <div class="border-2 h-36">playlist</div>
+    
+  </div>
+</main>
+
+<style>
+  .title {
+    font-size: 0.45rem;
+    line-height: 0;
+  }
+</style>
